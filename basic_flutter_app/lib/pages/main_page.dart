559ffed127a85d1cabe01b47d0e47dba5dbaf86b
johnny <johnny@johnnydeMacBook-Pro.local>
2019-07-31 14:01:03 +0800

    update

--- basic_flutter_app/lib/pages/main_page.dart
+++ basic_flutter_app/lib/pages/main_page.dart
@@ -1,29 +1,69 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sparkline/flutter_sparkline.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
-import 'collapsing_toolbar/collapsing_profile.dart';
-import 'collapsing_toolbar/collapsing_tab.dart';
-import 'shop_items_page.dart';
-import 'list_page.dart';
-import 'account_page.dart';
-import 'main_two.dart';
+import 'package:we_rate_dogs/drawer/exampleCustomMenu/ExampleCustomMenu.dart';
+import 'package:we_rate_dogs/drawer/exampleHiddenDrawer/example_hidden_drawer.dart';
+import 'package:we_rate_dogs/inner_drawer/main.dart';
+
 import '../data/data.dart';
+import 'account_page.dart';
+import 'list_page.dart';
+import 'main_two.dart';
+import 'shop_items_page.dart';
 
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+const String _markdownData = """# Markdown Example
+Markdown allows you to easily include formatted text, images, and even formatted Dart code in your app.
+## Styling
+Style text as _italic_, __bold__, or `inline code`.
+- Use bulleted lists
+- To better clarify
+- Your points
+## Links
+You can use [hyperlinks](hyperlink) in markdown
+## Images
+You can include images:
+![Flutter logo](https://flutter.io/images/flutter-mark-square-100.png#100x100)
+## Markdown widget
+This is an example of how to create your own Markdown widget:
+    new Markdown(data: 'Hello _world_!');
+## Code blocks
+Formatted Dart code looks really pretty too:
+```
+void main() {
+  runApp(new MaterialApp(
+    home: new Scaffold(
+      body: new Markdown(data: markdownData)
+    )
+  ));
+}
+```
+Enjoy!
+""";
 
+
+class _MainPageState extends State<MainPage> {
   int currentIndex = 0;
   final charts = Data.charts;
+
+  TextEditingController _textFieldController = TextEditingController();
 
   static final List<String> chartDropdownItems = [
     'Last 7 days',
     'Last month',
     'Last year'
+  ];
+
+
+  static final List<String> drawer = [
+    'A',
+    'B',
+    'C'
   ];
   String actualDropdown = chartDropdownItems[0];
   int actualChart = 0;
@@ -31,14 +71,40 @@ class _MainPageState extends State<MainPage> {
 
   @override
   void initState() {
-     super.initState();
-     _pageController = PageController();
+    super.initState();
+    _pageController = PageController();
   }
+
   @override
   void dispose() {
-     super.dispose();
+    super.dispose();
     _pageController.dispose();
   }
+
+
+
+  _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('TextField in Dialog'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "TextField in Dialog"),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -95,58 +161,50 @@ class _MainPageState extends State<MainPage> {
           )
         ],
       ),
-      body:  StaggeredGridView.count(
+      body: StaggeredGridView.count(
         crossAxisCount: 2,
         crossAxisSpacing: 12.0,
         mainAxisSpacing: 12.0,
         padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         children: <Widget>[
           _buildTile(
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    new Container(
-//              margin: const EdgeInsets.all(15.0),
-                      padding: const EdgeInsets.all(1.0),
-                      decoration: new BoxDecoration(
-//                          border: new Border.all(color: Colors.blueAccent),
-
-//                  borderRadius: BorderRadius.all(
-//                    Radius.circular(25.0) //                 <--- border radius here
-//                ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new Container(
+                        padding: const EdgeInsets.all(1.0),
+                        decoration: new BoxDecoration(),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text('Total Views',
+                                style: TextStyle(color: Colors.blueAccent)),
+                            Text('265K',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 34.0))
+                          ],
+                        ),
                       ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text('Total Views',
-                              style: TextStyle(color: Colors.blueAccent)),
-                          Text('265K',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 34.0))
-                        ],
-                      ),
-                    ),
-                    Material(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(24.0),
-                        child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Icon(Icons.timeline,
-                                  color: Colors.white, size: 30.0),
-                            )))
-                  ]),
-            ),
+                      Material(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(24.0),
+                          child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Icon(Icons.timeline,
+                                    color: Colors.white, size: 30.0),
+                              )))
+                    ]),
+              ),
               onTap: () =>
                   Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (_) => Account()))
-          ),
+                      .push(MaterialPageRoute(builder: (_) => Account()))),
           _buildTile(
             Padding(
               padding: const EdgeInsets.all(24.0),
@@ -154,16 +212,60 @@ class _MainPageState extends State<MainPage> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Material(
-                        color: Colors.teal,
-                        shape: CircleBorder(),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Icon(Icons.settings_applications,
-                              color: Colors.white, size: 30.0),
-                        )),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Material(
+                            color: Colors.deepPurpleAccent,
+                            shape: CircleBorder(),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Icon(Icons.settings_applications,
+                                  color: Colors.white, size: 30.0),
+                            )),
+                        DropdownButton(
+                            isDense: true,
+                            // value: actualDropdown,
+                            onChanged: (String value) =>
+                                setState(() {
+                                  if ("A" == value) {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (_) =>
+                                                ExampleCustomMenu(
+                                                )));
+                                  } else if ("B" == value) {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (_) =>
+                                                ExampleHiddenDrawer(
+                                                )));
+                                  } else if ("C"== value){
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (_) =>
+                                                MyHomePage(title: "ok",
+                                                )));
+
+                                    //
+
+                                  }
+                                }),
+                            items: drawer.map((String title) {
+                              return DropdownMenuItem(
+                                value: title,
+                                child: Text(title,
+                                    style: TextStyle(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 14.0)),
+                              );
+                            }).toList())
+                      ],
+                    ),
                     Padding(padding: EdgeInsets.only(bottom: 16.0)),
-                    Text('General',
+                    Text('Drawer',
                         style: TextStyle(
                             color: Colors.black,
                             fontWeight: FontWeight.w700,
@@ -172,39 +274,96 @@ class _MainPageState extends State<MainPage> {
                         style: TextStyle(color: Colors.black45)),
                   ]),
             ),
-
-              onTap: () =>
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (_) => ListPage( title: "Lession", )))
+//              onTap: () =>Navigator.of(context).push(MaterialPageRoute(
+//    builder: (_) => ExampleCustomMenu(
+//    )))
           ),
           _buildTile(
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Material(
-                        color: Colors.amber,
-                        shape: CircleBorder(),
-                        child: Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Icon(Icons.notifications,
-                              color: Colors.white, size: 30.0),
-                        )),
-                    Padding(padding: EdgeInsets.only(bottom: 16.0)),
-                    Text('Alerts',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 24.0)),
-                    Text('All ', style: TextStyle(color: Colors.black45)),
-                  ]),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Material(
+                          color: Colors.deepOrange,
+                          shape: CircleBorder(),
+                          child: Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Icon(Icons.perm_media,
+                                color: Colors.white, size: 30.0),
+                          )),
+                      Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                      Text('Dailog',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 24.0)),
+                      Text('All ', style: TextStyle(color: Colors.black45)),
+                    ]),
+              ),
+              onTap: () =>
+              _displayDialog(context)
+
+          ),
+          _buildTile(
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Material(
+                          color: Colors.teal,
+                          shape: CircleBorder(),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Icon(Icons.settings_applications,
+                                color: Colors.white, size: 30.0),
+                          )),
+                      Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                      Text('General',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 24.0)),
+                      Text('Images, Videos',
+                          style: TextStyle(color: Colors.black45)),
+                    ]),
+              ),
+              onTap: () =>
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) =>
+                          ListPage(
+                            title: "Lession",
+                          )))),
+          _buildTile(
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Material(
+                          color: Colors.amber,
+                          shape: CircleBorder(),
+                          child: Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Icon(Icons.notifications,
+                                color: Colors.white, size: 30.0),
+                          )),
+                      Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                      Text('Alerts',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 24.0)),
+                      Text('All ', style: TextStyle(color: Colors.black45)),
+                    ]),
+              ),
               onTap: () =>
                   Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (_) => Dashboard()))
-          ),
+                      .push(MaterialPageRoute(builder: (_) => Dashboard()))),
           _buildTile(
             Padding(
                 padding: const EdgeInsets.all(24.0),
@@ -299,6 +458,8 @@ class _MainPageState extends State<MainPage> {
           StaggeredTile.extent(2, 110.0),
           StaggeredTile.extent(1, 180.0),
           StaggeredTile.extent(1, 180.0),
+          StaggeredTile.extent(1, 180.0),
+          StaggeredTile.extent(1, 180.0),
           StaggeredTile.extent(2, 220.0),
           StaggeredTile.extent(2, 110.0),
         ],
@@ -318,31 +479,26 @@ class _MainPageState extends State<MainPage> {
           BottomNavyBarItem(
               icon: Icon(Icons.people),
               title: Text('Users'),
-              activeColor: Colors.purpleAccent
-          ),
+              activeColor: Colors.purpleAccent),
           BottomNavyBarItem(
               icon: Icon(Icons.message),
               title: Text('Messages'),
-              activeColor: Colors.pink
-          ),
+              activeColor: Colors.pink),
           BottomNavyBarItem(
               icon: Icon(Icons.settings),
               title: Text('Settings'),
-              activeColor: Colors.blue
-          ),
+              activeColor: Colors.blue),
         ],
       ),
-
-    )
-    ;
+    );
   }
 
   Widget _buildTile(Widget child, {Function() onTap}) {
     return Material(
         elevation: 14.0, //
-    borderRadius: BorderRadius.circular(12.0),
-    shadowColor: Color(0x802196F3),
-    child: InkWell(
+        borderRadius: BorderRadius.circular(12.0),
+        shadowColor: Color(0x802196F3),
+        child: InkWell(
           // Do onTap() if it isn't null, otherwise do print()
             onTap: onTap != null
                 ? () => onTap()
